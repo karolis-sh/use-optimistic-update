be2001e5af9077931cf9e8dab89d232667b15823
Karolis Šarapnickis <pastas.k@gmail.com>
2020-05-05 00:47:52 +0300

    test: add test for identical optimist update check

--- src/optimist.test.ts
+++ src/optimist.test.ts
@@ -72,3 +72,41 @@ it('should handle consecutive updates', async () => {
     { isUpdating: false, value: -1 },
   ]);
 });
+
+it('should handle identical updates', async () => {
+  const optimist = new Optimist();
+  const key = 'test-1';
+
+  let state: State | undefined;
+  const history: State[] = [];
+
+  optimist.onUpdate(key, (newState) => {
+    state = newState;
+    history.push(newState);
+  });
+
+  expect(state).toBeUndefined();
+
+  optimist.sync(key, 1);
+  expect(state).toEqual({ isUpdating: false, value: 1 });
+
+  optimist.update(key, delay, 2);
+  expect(state).toEqual({ isUpdating: true, value: 2 });
+
+  optimist.update(key, delay, 2);
+  expect(state).toEqual({ isUpdating: true, value: 2 });
+
+  await delay();
+  expect(state).toEqual({ isUpdating: true, value: 2 });
+  optimist.sync(key, -999);
+
+  await delay();
+  expect(state).toEqual({ isUpdating: false, value: -999 });
+
+  expect(history).toEqual([
+    { isUpdating: false, value: 1 },
+    { isUpdating: false, value: 2 },
+    { isUpdating: true, value: 2 },
+    { isUpdating: false, value: -999 },
+  ]);
+});
