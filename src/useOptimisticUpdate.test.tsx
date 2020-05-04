9ae7323805c7129fdd1b3771f36afa7694c79765
Karolis Šarapnickis <pastas.k@gmail.com>
2020-05-04 22:28:59 +0300

    test: add simple test

--- src/useOptimisticUpdate.test.tsx
+++ src/useOptimisticUpdate.test.tsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 
-const UpTo5Counter: React.FC = () => {
+import useOptimisticUpdate from './useOptimisticUpdate';
+
+const DELAY = 5;
+
+const sleep = (): Promise<void> => new Promise((resolve) => setTimeout(resolve, DELAY + 5));
+
+const AsyncUpTo3Counter: React.FC = () => {
   const [counter, setCounter] = useState(0);
 
-  const increment = (value: number): Promise<void> =>
+  const increment = (newValue: number): Promise<void> =>
     new Promise((resolve) => {
       setTimeout(() => {
-        if (value <= 5) setCounter(value);
+        if (newValue <= 3) setCounter(newValue);
         resolve();
-      }, 600);
+      }, DELAY);
     });
 
   return (
@@ -25,10 +31,80 @@ const UpTo5Counter: React.FC = () => {
   );
 };
 
-test('should render button', () => {
-  render(<UpTo5Counter />);
-  const button = screen.getByTestId('button');
+const OptimisticUpTo3Counter: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+  const { value: optimisticCounter, onUpdate } = useOptimisticUpdate('UpTo3Counter', counter);
 
-  expect(button).toBeInTheDocument();
+  const increment = (newValue: number): Promise<void> =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        if (newValue <= 3) setCounter(newValue);
+        resolve();
+      }, DELAY);
+    });
+
+  return (
+    <button
+      data-testid="button"
+      type="button"
+      onClick={(): void => {
+        const newValue = (optimisticCounter as number) + 1;
+        onUpdate(() => increment(newValue), newValue);
+      }}
+    >
+      Likes: {optimisticCounter}
+    </button>
+  );
+};
+
+it('should have delayed render on AsyncUpTo3Counter interactions', async () => {
+  render(<AsyncUpTo3Counter />);
+  const button = screen.getByTestId('button');
   expect(button.textContent).toBe('Likes: 0');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 0');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 1');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 1');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 2');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 2');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 3');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 3');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 3');
+});
+
+it('should pre-render optimistic values on OptimisticUpTo3Counter interactions', async () => {
+  render(<OptimisticUpTo3Counter />);
+  const button = screen.getByTestId('button');
+  expect(button.textContent).toBe('Likes: 0');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 1');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 1');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 2');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 2');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 3');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 3');
+
+  fireEvent.click(screen.getByTestId('button'));
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 4');
+  await act(() => sleep());
+  expect(screen.getByTestId('button').textContent).toBe('Likes: 3');
 });
